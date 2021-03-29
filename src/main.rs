57cb8dbd253739e945b0ce8a20b4d8613d7929c4
Janis Strautins <strautins.janis@gmail.com>
2021-03-29 17:06:15 +0300

    Final change

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,18 @@
-mod sonar_state;
 mod sonar;
+mod sonar_state;
 use sonar_state::StatusState;
 use std::thread;
 
+use rodio::Source;
 use std::fs::File;
 use std::io::BufReader;
-use rodio::Source;
-use std::time::{Duration};
+use std::time::Duration;
 
 fn main() {
     let (_stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
 
     // Sonar setup
-    let mut sonar_state = match sonar_state::SonarState::new()
-    {
+    let mut sonar_state = match sonar_state::SonarState::new() {
         Some(state) => state,
         None => {
             println!("Failed to create state");
@@ -22,7 +21,6 @@ fn main() {
     };
 
     let mut last_state: StatusState = sonar_state.get_state();
-    
     loop {
         sonar_state.state_tick();
         let state: StatusState = sonar_state.get_state();
@@ -35,13 +33,13 @@ fn main() {
                 if state != last_state {
                     last_state = state;
                     // This works fine, except can't be used to play a song, as I wont be able to stop it
-                    // thread::spawn(|| {
-                    //     play::play("audio/welcome.mp3").unwrap();
-                    // });
+                    thread::spawn(|| {
+                        play::play("audio/welcome.mp3").unwrap();
+                    });
 
-                    let file = File::open("audio/welcome.mp3").unwrap();
-                    let source = rodio::Decoder::new(BufReader::new(file)).unwrap();
-                    stream_handle.play_raw(source.convert_samples()).unwrap();
+                    // let file = File::open("audio/welcome.mp3").unwrap();
+                    // let source = rodio::Decoder::new(BufReader::new(file)).unwrap();
+                    // stream_handle.play_raw(source.convert_samples()).unwrap();
                 }
             }
             StatusState::OffTrigger => {
@@ -51,18 +49,15 @@ fn main() {
                 if state != last_state {
                     last_state = state;
 
-                    // thread::spawn(|| {
-                    //     play::play("audio/bye.mp3").unwrap();
-                    // });
+                    thread::spawn(|| {
+                        play::play("audio/bye.mp3").unwrap();
+                    });
 
-                    let file = File::open("audio/bye.mp3").unwrap();
-                    let source = rodio::Decoder::new(BufReader::new(file)).unwrap();
-                    stream_handle.play_raw(source.convert_samples()).unwrap();
-                }   
+                    // let file = File::open("audio/bye.mp3").unwrap();
+                    // let source = rodio::Decoder::new(BufReader::new(file)).unwrap();
+                    // stream_handle.play_raw(source.convert_samples()).unwrap();
+                }
             }
         }
-
-        // For Pi3
-        thread::sleep(Duration::from_millis(100));
     }
 }
